2c015092d33bb2a06d95aca6c61e12efe5a0f352
Rachel Long <rachellong@Rachels-MacBook-Pro.local>
2020-01-03 11:00:50 -0500

    working search by neighborhood

--- script.js
+++ script.js
@@ -5,45 +5,48 @@ async function findLocation(borough) {
     let findBoro = await axios.get(`https://data.cityofnewyork.us/resource/qnjm-wvu5.json?borough=${borough}`);
 
     
-    async function findCloseLocation(neighborhood) {
+        async function findCloseLocation(neighborhood) {
       
-    let findNeighborhood = await axios.get(`https://data.cityofnewyork.us/resource/qnjm-wvu5.json?ntaname=${neighborhood}`);
+        let findNeighborhood = await axios.get(`https://data.cityofnewyork.us/resource/qnjm-wvu5.json?ntaname=${neighborhood}`);
          
-    try {
-        document.querySelector('h2').innerHTML = "address: " + findNeighborhood.data[0].address;
-        document.querySelector('h3').innerHTML = "name: " + findNeighborhood.data[0].vendor_name;
-        document.querySelector('h4').innerHTML = findNeighborhood.data[0].website;
+        try {
+         document.querySelector('h2').innerHTML = "address: " + findNeighborhood.data[0].address;
+         document.querySelector('h3').innerHTML = "name: " + findNeighborhood.data[0].vendor_name;
+         document.querySelector('h4').innerHTML = findNeighborhood.data[0].website;
         
-        console.log(findNeighborhood.data)
+         console.log(findNeighborhood.data)
     
 
-    return findNeighborhood;
-    } catch(err) {
-     console.log(`Oops! Error occurred: ${err}`)
-    console.log(err.response)
-}
-}
+        return findNeighborhood;
+     } catch(err) {
+         console.log(`Oops! Error occurred: ${err}`)
+        console.log(err.response)
+        }
+        }
 
-
+        let select = document.querySelector('#neighborhood')
         //pulls neighborhood data from boro search and adds to drop down list
-        let option = document.querySelector('option')
-        // document.write("<select name='neighborhood' onchange='this.form.submit()'>");
+        let elm = document.getElementById('neighborhood')
+        
+        df = document.createDocumentFragment()
+            for(let i = 0; i < 100; i++){
+            let option = document.createElement('option')
+            option.value = findBoro.data[i].ntaname
+            option.appendChild(document.createTextNode(findBoro.data[i].ntaname))
+            df.appendChild(option)
+            elm.appendChild(df)
 
-       for(let i = 0; i < 100; i++){
-
-         option.innerHTML = findBoro.data[i].ntaname 
-        // document.write("<option>" + findBoro.data[i].ntaname + "</option>");
-
-        console.log(findBoro.data[i].ntaname)
+            
+            console.log(findBoro.data[i].ntaname)
        }
- // dropdown search by neighborhood
-     let neighborhoodInput = document.querySelector('#hood')
+
+ // dropdown search by neighborhood//////////////////////////////////////////////////////
      let neighborhoodSubmit = document.querySelector('#neighborhoodButton')
 
     
       const chooseNeighb = function(event) {
        event.preventDefault()
-        let neighbData = neighborhoodInput.value
+        let neighbData = select.value
          findCloseLocation(neighbData)
      }
      neighborhoodSubmit.addEventListener('click', chooseNeighb)
